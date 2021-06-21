ea6bb3cdf8519e69ec6918746bd10c57e238fe41
RKarlhuber <r.karlhuber@outlook.com>
2021-06-21 09:26:39 +0200

    assets and function

---
+++ Assets/MyGame/Scripts/PickupManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupManager : MonoBehaviour
+{
+    [SerializeField] int pickupCount;
+    [SerializeField] GameObject endDoorGO;
+    [SerializeField] GameObject endDoorOpenGO;
+
+    public void CollectPickup ()
+    {
+        pickupCount +=1;
+
+        if (pickupCount==3)
+        {
+            // Debug.Log("alle 3 gesammelt");
+            endDoorGO.SetActive(false);
+            endDoorOpenGO.SetActive(true);
+
+        }
+    }
+}
